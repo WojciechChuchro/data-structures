4a734fecd4f4921eca5a33fcca3dae721d897b04
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-03-22 11:00:44 +0100

    feat: implement new methods using only head - getFirst - size - removeFirst - removeLastFromHead - getLastFromHead - set proper function names

--- includes/SingleLinkedList.h
+++ includes/SingleLinkedList.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdbool.h>
 
 #ifndef SINGLELINKEDLIST_H
@@ -9,8 +11,14 @@ typedef struct SingleLinkedList {
 } SingleLinkedList;
 SingleLinkedList* createSingleLinkedList(int val);
 
-bool add(SingleLinkedList** head, int val);
+bool addFromHead(SingleLinkedList** head, int val);
+bool addFirst(SingleLinkedList** head, int val);
 void printFromHead(SingleLinkedList** head);
+SingleLinkedList* getFirst(SingleLinkedList* head);
+int size(SingleLinkedList* head);
+bool removeFirst(SingleLinkedList** head);
+bool removeLastFromHead(SingleLinkedList** head);
+SingleLinkedList* getLastFromHead(SingleLinkedList** head);
 
 
 #endif //SINGLELINKEDLIST_H
--- main.c
+++ main.c
@@ -2,10 +2,42 @@
 #include <stdlib.h>
 #include "includes/SingleLinkedList.h"
 
-int main(void) {
+void test(SingleLinkedList* head) {
+    SingleLinkedList* h = createSingleLinkedList(10);
+
+    head->val = 15;
+    head->next = h;
+
+    printf("test: %d", head->val);
+}
+SingleLinkedList* init() {
     SingleLinkedList* head = createSingleLinkedList(10);
 
+    addFromHead(&head, 15);
+    addFromHead(&head, 1);
+    addFromHead(&head, 2);
+    addFromHead(&head, 3);
+
+    printf("Init list\n");
     printFromHead(&head);
+
+    return head;
+}
+
+int main(void) {
+    SingleLinkedList* head = init();
+    SingleLinkedList* lastNode = NULL;
+
+    // addFirst(&head, 69);
+    // getFirst(head);
+
+    // removeFirst(&head);
+    // lastNode = getLastFromHead(&head);
+    removeLastFromHead(&head);
+    printFromHead(&head);
+    // const int s = size(head);
+    // printf("size %d", s);
+
     free(head);
     return 0;
 }
